eda947d77db4463cb5e919b5dad903619cd5cdfb
stdstring <std_string@mail.ru>
2026-02-27 00:08:25 +0500

    started creation of python porter app

---
+++ information.security/diploma/apps/PythonPorter/PythonPorterApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PythonPorterApp
+{
+    internal class Program
+    {
+        static Int32 Main(string[] args)
+        {
+            Console.WriteLine("Python porter app");
+            return 0;
+        }
+    }
+}
---
+++ information.security/diploma/apps/PythonPorter/PythonPorterAppTests/PythonPorterAppIntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using PythonPorterAppTests.TestUtils;
+
+namespace PythonPorterAppTests
+{
+    [TestFixture]
+    public class PythonPorterAppIntegrationTests
+    {
+        [Test]
+        public void Check()
+        {
+            ExecutionResult result = ExecutionHelper.Execute("");
+            Assert.That(result.ExitCode, Is.EqualTo(0));
+        }
+    }
+}
